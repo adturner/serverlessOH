3e30eb69ec0d5a68e89d829cc34a2a0a39a2b0dc
samleung314 <37159333+samleung314@users.noreply.github.com>
2021-12-14 17:11:35 -0500

    create cosmosdb binding

--- RatingsAPI/GetRatings.cs
+++ RatingsAPI/GetRatings.cs
@@ -13,23 +13,46 @@ namespace RatingsAPI
     public static class GetRatings
     {
         [FunctionName("GetRatings")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        public static IActionResult Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)]
+                HttpRequest req,
+            [CosmosDB(
+                databaseName: "products",
+                collectionName: "ratings",
+                ConnectionStringSetting = "CosmosDBConnection",
+                Id = "{Query.id}",
+                PartitionKey = "productId")] Rating ratingItem, //"{Query.partitionKey}")
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
+            if (ratingItem == null)
+            {
+                log.LogInformation($"ratingItem not found");
+            }
+            else
+            {
+                log.LogInformation($"Found ratings item, Description={ratingItem.userNotes}");
+            }
+            return new OkResult();
         }
+        // public static async Task<IActionResult> Run(
+        //     [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+        //     ILogger log)
+        // {
+        //     log.LogInformation("C# HTTP trigger function processed a request.");
+
+        //     string name = req.Query["name"];
+
+        //     string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        //     dynamic data = JsonConvert.DeserializeObject(requestBody);
+        //     name = name ?? data?.name;
+
+        //     string responseMessage = string.IsNullOrEmpty(name)
+        //         ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+        //         : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+        //     return new OkObjectResult(responseMessage);
+        // }
     }
 }
